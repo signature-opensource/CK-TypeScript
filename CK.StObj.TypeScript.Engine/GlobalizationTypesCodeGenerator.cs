69319c10b506eb6f45535eec5acea3cecbf7855b
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-09-17 11:30:49 +0200

    Added doc to SimpleUserMessage TypeScript.

--- CK.StObj.TypeScript.Engine/GlobalizationTypesCodeGenerator.cs
+++ CK.StObj.TypeScript.Engine/GlobalizationTypesCodeGenerator.cs
@@ -36,14 +36,27 @@ namespace CK.StObj.TypeScript.Engine
 
                 var part = file.EnsureTypePart( closer: "" );
                 part.Append( @"
-export class SimpleUserMessage {
-    
-    constructor(public readonly level: UserMessageLevel, public readonly message: string, public readonly depth: number) {}
+/**
+ * Simple info, warn or error message with an optional indentation.
+ **/
+export class SimpleUserMessage
+{
+    /**
+     * @param message Message level (info, warn or error). 
+     * @param message Message text. 
+     * @param message Optional indentation. 
+    **/
+    constructor(
+        public readonly level: UserMessageLevel,
+        public readonly message: string,
+        public readonly depth: number = 0
+    ) {}
 
     toString() {
         return '['+UserMessageLevel[this.level]+'] ' + this.message;
       }
-}" );
+}
+" );
             }
             return true;
         }
