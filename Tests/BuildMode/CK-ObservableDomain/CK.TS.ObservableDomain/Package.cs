0ef30734b3f90ff69a23b6f3b4760f92fb437d55
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-06-13 11:12:59 +0200

    Restored lost tests.

--- Tests/BuildMode/CK-ObservableDomain/CK.TS.ObservableDomain/Package.cs
+++ Tests/BuildMode/CK-ObservableDomain/CK.TS.ObservableDomain/Package.cs
@@ -5,8 +5,8 @@ namespace CK.ObservableDomain
     [TypeScriptPackage]
     [ImportTypeScriptLibrary("rxjs", "7.5.6", DependencyKind.Dependency)]
     [TypeScriptFile( "IObservableDomainLeagueDriver.ts", "IObservableDomainLeagueDriver" )]
-    [TypeScriptFile( "ObservableDomain.ts", "WatchEvent", "ObservableDomain" )]
-    [TypeScriptFile( "ObservableDomainClient.ts", "ObservableDomainClientConnectionState", "ObservableDomainClient" )]
+    [TypeScriptFile( "ObservableDomain.ts", "ObservableDomain", "WatchEvent" )]
+    [TypeScriptFile( "ObservableDomainClient.ts", "ObservableDomainClient", "ObservableDomainClientConnectionState" )]
     public class Package : TypeScriptPackage
     {
         void StObjConstruct( CK.JsonGraphSerializer.Package graphSerializer ) { }
