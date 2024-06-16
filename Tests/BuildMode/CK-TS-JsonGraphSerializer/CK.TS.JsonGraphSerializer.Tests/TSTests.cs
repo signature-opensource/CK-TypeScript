501ccdf7004aff29271bc3f66c1de6b760ae8269
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-06-16 08:28:57 +0200

    Cleanup dependency to @local/ck-gen.
    It only needs to be a devDependency.
    Updated CK_TS_JsonGraphSerializer:  No Roslyn compilation
    => -2 seconds.
    -

--- Tests/BuildMode/CK-TS-JsonGraphSerializer/CK.TS.JsonGraphSerializer.Tests/TSTests.cs
+++ Tests/BuildMode/CK-TS-JsonGraphSerializer/CK.TS.JsonGraphSerializer.Tests/TSTests.cs
@@ -2,6 +2,8 @@ using NUnit.Framework;
 using System.Threading.Tasks;
 using CK.Testing;
 using static CK.Testing.StObjEngineTestHelper;
+using CK.Setup;
+using FluentAssertions;
 
 namespace CK.TS.JsonGraphSerializer.Tests
 {
@@ -11,8 +13,22 @@ namespace CK.TS.JsonGraphSerializer.Tests
         [Test]
         public async Task CK_TS_JsonGraphSerializer_Async()
         {
+            // We don't need any C# backend here. Instead of this:
+            //
+            // var targetProjectPath = TestHelper.GetTypeScriptBuildModeTargetProjectPath();
+            // TestHelper.RunSuccessfulEngineWithTypeScript( targetProjectPath, typeof( CK.JsonGraphSerializer.TSPackage ).Assembly );
+            //
+            // Skipping the Roslyn compilation saves 2 seconds.
+            //
             var targetProjectPath = TestHelper.GetTypeScriptBuildModeTargetProjectPath();
-            TestHelper.RunSuccessfulEngineWithTypeScript( targetProjectPath, typeof( CK.JsonGraphSerializer.TSPackage ).Assembly );
+
+            var engineConfig = TestHelper.CreateDefaultEngineConfiguration( compileOption: CompileOption.None );
+            TestHelper.EnsureTypeScriptConfigurationAspect( engineConfig, targetProjectPath );
+            var types = TestHelper.CreateTypeCollector( typeof( CK.JsonGraphSerializer.TSPackage ).Assembly );
+
+            TestHelper.RunEngine( engineConfig, types ).Success.Should().BeTrue( "Engine.Run worked." );
+
+            // Runs the Jest tests.
             await using var runner = TestHelper.CreateTypeScriptRunner( targetProjectPath );
             runner.Run();
         }
