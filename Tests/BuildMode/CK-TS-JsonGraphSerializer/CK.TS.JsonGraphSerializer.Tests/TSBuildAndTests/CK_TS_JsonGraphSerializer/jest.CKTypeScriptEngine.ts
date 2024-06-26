ad8589a3bf96402483fa07c3405efdce9f1bd6bd
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-06-26 13:39:53 +0200

    Minor cleanup.

--- Tests/BuildMode/CK-TS-JsonGraphSerializer/CK.TS.JsonGraphSerializer.Tests/TSBuildAndTests/CK_TS_JsonGraphSerializer/jest.CKTypeScriptEngine.ts
+++ Tests/BuildMode/CK-TS-JsonGraphSerializer/CK.TS.JsonGraphSerializer.Tests/TSBuildAndTests/CK_TS_JsonGraphSerializer/jest.CKTypeScriptEngine.ts
@@ -8,7 +8,3 @@
 import { TextDecoder as ImportedTextDecoder, TextEncoder as ImportedTextEncoder, } from "util";
 Object.assign(global, { TextDecoder: ImportedTextDecoder, TextEncoder: ImportedTextEncoder, })
 // </fix>
-// And this is another one:
-// <fix href="https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios/43020260#43020260">
-Object.assign(global, { XMLHttpRequest: undefined });
-// </fix>
--- Tests/CK.TS.AspNet.Auth.Tests/TSBuildAndTests/CK_TS_AspNet_Auth/jest.CKTypeScriptEngine.ts
+++ Tests/CK.TS.AspNet.Auth.Tests/TSBuildAndTests/CK_TS_AspNet_Auth/jest.CKTypeScriptEngine.ts
@@ -8,9 +8,3 @@
 import { TextDecoder as ImportedTextDecoder, TextEncoder as ImportedTextEncoder, } from "util";
 Object.assign(global, { TextDecoder: ImportedTextDecoder, TextEncoder: ImportedTextEncoder, })
 // </fix>
-// And this is another one:
-// <fix href="https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios/43020260#43020260">
-//Object.assign(global, { XMLHttpRequest: undefined });
-// </fix>
-
-Object.assign( process.env, {"SERVER_ADDRESS":"http://[::1]:7494","CK_TYPESCRIPT_ENGINE":"true"});
