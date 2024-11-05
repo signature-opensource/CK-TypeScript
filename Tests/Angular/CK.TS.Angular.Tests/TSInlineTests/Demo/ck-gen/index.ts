ea18040f0a8b187f367bf492e750d2fa59b71866
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-11-05 16:14:45 +0100

    Removed ck-gen folder.

--- Tests/Angular/CK.TS.Angular.Tests/TSInlineTests/Demo/ck-gen/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './CK/NG/AspNet/Auth/password-lost/password-lost.component';
-export * from './CK/NG/AspNet/Auth/login/login.component';
-export * from './CK/Demo/demo-ng/demo-ng.module';
-export * from './CK/Angular/CKGenAppModule';
-export * from './CK/Core/CTSType';
--- Tests/BuildMode/CK-ObservableDomain/CK.TS.ObservableDomain.Tests/TSInlineTests/CK_TS_ObservableDomain/ck-gen/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './CK/ObservableDomain/ObservableDomainClient';
-export * from './CK/ObservableDomain/ObservableDomain';
-export * from './CK/ObservableDomain/IObservableDomainLeagueDriver';
-export * from './CK/JsonGraphSerializer';
-export * from './CK/Core/CTSType';
