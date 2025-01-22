8c1752636bafb507d325580865ad987cd1ad43a1
Arthur Cheng <arthur.cheng@signature.one>
2025-01-22 17:07:01 +0100

    upgraded Angular version to ^19, and yarn version to 4.6.0

--- Tests/Angular/CK.TS.Angular.Tests/CK.Ng.PublicSection/PublicSectionPackage.cs
+++ Tests/Angular/CK.TS.Angular.Tests/CK.Ng.PublicSection/PublicSectionPackage.cs
@@ -3,17 +3,11 @@ using CK.TypeScript;
 
 namespace CK.Ng.PublicSection;
 
-
 [TypeScriptPackage]
-public sealed partial class PublicSectionPackage : TypeScriptPackage
-{
-}
-
-// Test the LibraryName + SubPath.
 [TypeScriptImportLibrary( "ng-zorro-antd", "^19", DependencyKind.Dependency, ForceUse = true )]
-[NgProviderImport( "NZ_I18N", LibraryName = "ng-zorro-antd/i18n" )]
-[NgProviderImport( "fr_FR", LibraryName = "ng-zorro-antd/i18n" )]
+
+[NgProviderImport( "fr_FR, NZ_I18N", LibraryName = "ng-zorro-antd/i18n" )]
 [NgProvider( "{ provide: NZ_I18N, useValue: fr_FR }" )]
-public sealed partial class PublicSectionPackage : TypeScriptPackage
+public sealed class PublicSectionPackage : TypeScriptPackage
 {
 }
