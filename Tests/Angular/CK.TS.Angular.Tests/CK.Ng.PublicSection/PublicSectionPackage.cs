2f734a3a4034c02789e0fbf01bacad01b5d762d5
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-01-22 16:33:31 +0100

    Fix [NgProviderImport] LibraryName + SubPath.

--- Tests/Angular/CK.TS.Angular.Tests/CK.Ng.PublicSection/PublicSectionPackage.cs
+++ Tests/Angular/CK.TS.Angular.Tests/CK.Ng.PublicSection/PublicSectionPackage.cs
@@ -1,8 +1,19 @@
+using CK.TS.Angular;
 using CK.TypeScript;
 
 namespace CK.Ng.PublicSection;
 
+
 [TypeScriptPackage]
-public sealed class PublicSectionPackage : TypeScriptPackage
+public sealed partial class PublicSectionPackage : TypeScriptPackage
+{
+}
+
+// Test the LibraryName + SubPath.
+[TypeScriptImportLibrary( "ng-zorro-antd", "^19", DependencyKind.Dependency, ForceUse = true )]
+[NgProviderImport( "NZ_I18N", LibraryName = "ng-zorro-antd/i18n" )]
+[NgProviderImport( "fr_FR", LibraryName = "ng-zorro-antd/i18n" )]
+[NgProvider( "{ provide: NZ_I18N, useValue: fr_FR }" )]
+public sealed partial class PublicSectionPackage : TypeScriptPackage
 {
 }
