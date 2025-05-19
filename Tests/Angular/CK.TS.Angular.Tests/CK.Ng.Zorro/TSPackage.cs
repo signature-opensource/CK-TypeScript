f879185653f27c053ed9e27fc5e6e998e640e504
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-05-19 15:21:07 +0200

    Fix yarn.lock.

--- Tests/Angular/CK.TS.Angular.Tests/CK.Ng.Zorro/TSPackage.cs
+++ Tests/Angular/CK.TS.Angular.Tests/CK.Ng.Zorro/TSPackage.cs
@@ -5,8 +5,6 @@ namespace CK.Ng.Zorro;
 
 [TypeScriptPackage]
 [TypeScriptImportLibrary( "ng-zorro-antd", "^19", DependencyKind.Dependency, ForceUse = true )]
-[NgProviderImport( "fr_FR, NZ_I18N", LibraryName = "ng-zorro-antd/i18n" )]
-[NgProvider( "{ provide: NZ_I18N, useValue: fr_FR }" )]
 [AppStyleImport( "ng-zorro-antd/ng-zorro-antd.less" )]
 public class TSPackage : TypeScriptPackage
 {
