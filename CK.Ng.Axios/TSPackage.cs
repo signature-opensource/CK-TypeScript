b66a1b6c8430b57d3cff6ab516091e78fec96def
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-06-04 08:37:39 +0200

    Use package mapping instead of unitary file mapping.

--- CK.Ng.Axios/TSPackage.cs
+++ CK.Ng.Axios/TSPackage.cs
@@ -7,9 +7,9 @@ namespace CK.Ng.Axios;
 /// Imports axios library and provides the Angular AXIOS injection token
 /// for the default AxiosInstance to use.
 /// </summary>
-[TypeScriptPackage]
+[TypeScriptPackage( TypeScriptFolder = "CK/Ng" )]
 [TypeScriptImportLibrary( "axios", "^1.7.7", DependencyKind.PeerDependency, ForceUse = true )]
-[TypeScriptFile( "AXIOSToken.ts", "AXIOS", TargetFolder ="CK/Ng" )]
+[TypeScriptFile( "AXIOSToken.ts", "AXIOS" )]
 [NgProviderImport( "AXIOS" )]
 [NgProviderImport( "default axios, AxiosInstance", LibraryName = "axios" )]
 [NgProvider( "{ provide: AXIOS, useValue: axios.create() }" )]
