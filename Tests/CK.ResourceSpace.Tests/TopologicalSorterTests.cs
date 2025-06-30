0e09a886db339c490b4b0f66f1de3c83d27129b2
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-06-30 14:23:21 +0200

    Added test from types.

--- Tests/CK.ResourceSpace.Tests/TopologicalSorterTests.cs
+++ Tests/CK.ResourceSpace.Tests/TopologicalSorterTests.cs
@@ -3,12 +3,13 @@ using CK.EmbeddedResources;
 using NUnit.Framework;
 using Shouldly;
 using System.Linq;
+using System.Runtime.InteropServices;
 using static CK.Testing.MonitorTestHelper;
 
 namespace CK.ResourceSpace.Tests;
 
 [TestFixture]
-public class TopologicalSorterTests
+public partial class TopologicalSorterTests
 {
     static ResPackageDescriptor CreatePackage( ResSpaceConfiguration config, string name )
     {
@@ -150,6 +151,8 @@ public class TopologicalSorterTests
 
     [TestCase( true, "RequiresAndChild" )]
     [TestCase( false, "RequiresAndChild" )]
+    [TestCase( true, "FromType" )]
+    [TestCase( false, "FromType" )]
     public void TS_Angular_structure( bool revertOrdering, string regType )
     {
         /*
@@ -174,48 +177,9 @@ public class TopologicalSorterTests
             <App> => AppRoutedComponent, DemoNgModule, PublicSectionComponent, SomeAuthPackage
          */
 
-        var config = new ResSpaceConfiguration();
-        config.RevertOrderingNames = revertOrdering;
-        var dAppRoutedComponent = CreatePackage( config, "AppRoutedComponent" );
-        var dDemoNgModule = CreatePackage( config, "DemoNgModule" );
-        var dPublicPageComponent = CreatePackage( config, "PublicPageComponent" );
-        var dZorro = CreatePackage( config, "Zorro" );
-        var dLogoutConfirmComponent = CreatePackage( config, "LogoutConfirmComponent" );
-        var dLoginComponent = CreatePackage( config, "LoginComponent" );
-        var dLogoutResultComponent = CreatePackage( config, "LogoutResultComponent" );
-        var dPublicFooterComponent = CreatePackage( config, "PublicFooterComponent" );
-        var dPublicTopbarComponent = CreatePackage( config, "PublicTopbarComponent" );
-        var dPasswordLostComponent = CreatePackage( config, "PasswordLostComponent" );
-        var dPublicSectionComponent = CreatePackage( config, "PublicSectionComponent" );
-        var dSomeAuthPackage = CreatePackage( config, "SomeAuthPackage" );
-
-        if( regType == "RequiresAndChild" )
-        {
-            dLoginComponent.Requires.Add( dPublicPageComponent );
-            dLogoutResultComponent.Requires.Add( dLogoutConfirmComponent );
-            dPasswordLostComponent.Requires.Add( dLoginComponent );
-            dPublicSectionComponent.Requires.Add( dZorro );
-            dPublicSectionComponent.Children.Add( dPublicTopbarComponent );
-            dPublicSectionComponent.Children.Add( dPublicFooterComponent );
-            dSomeAuthPackage.Children.Add( dPasswordLostComponent );
-            dSomeAuthPackage.Children.Add( dLogoutResultComponent );
-            dSomeAuthPackage.Children.Add( dLogoutConfirmComponent );
-            dSomeAuthPackage.Children.Add( dLoginComponent );
-        }
-        else
-        {
-            // TODO !
-            dLoginComponent.Requires.Add( dPublicPageComponent );
-            dLogoutResultComponent.Requires.Add( dLogoutConfirmComponent );
-            dPasswordLostComponent.Requires.Add( dLoginComponent );
-            dPublicSectionComponent.Requires.Add( dZorro );
-            dPublicSectionComponent.Children.Add( dPublicTopbarComponent );
-            dPublicSectionComponent.Children.Add( dPublicFooterComponent );
-            dSomeAuthPackage.Children.Add( dPasswordLostComponent );
-            dSomeAuthPackage.Children.Add( dLogoutResultComponent );
-            dSomeAuthPackage.Children.Add( dLogoutConfirmComponent );
-            dSomeAuthPackage.Children.Add( dLoginComponent );
-        }
+        ResSpaceConfiguration config = regType == "RequiresAndChild"
+                                        ? ExplicitGraph( revertOrdering )
+                                        : FromType( revertOrdering );
 
         var collector = config.Build( TestHelper.Monitor ).ShouldNotBeNull();
         var builder = new ResSpaceDataBuilder( collector );
@@ -270,35 +234,83 @@ public class TopologicalSorterTests
 
             spaceData.AllPackageResources
                     .Select( p => p.ToString() ).ShouldBe( [
-                        "Before <Code>" ,
-                        "After <Code>" ,
-                        "Before Zorro" ,
-                        "After Zorro" ,
-                        "Before SomeAuthPackage" ,
-                            "Before PublicPageComponent" ,
-                            "After PublicPageComponent" ,
-                            "Before LoginComponent" ,
-                            "After LoginComponent" ,
-                            "Before PasswordLostComponent" ,
-                            "After PasswordLostComponent" ,
-                            "Before LogoutConfirmComponent" ,
-                            "After LogoutConfirmComponent" ,
-                            "Before LogoutResultComponent" ,
-                            "After LogoutResultComponent" ,
-                        "After SomeAuthPackage" ,
-                        "Before PublicSectionComponent" ,
-                            "Before PublicTopbarComponent" ,
-                            "After PublicTopbarComponent" ,
-                            "Before PublicFooterComponent" ,
-                            "After PublicFooterComponent" ,
-                        "After PublicSectionComponent" ,
-                        "Before DemoNgModule" ,
-                        "After DemoNgModule" ,
-                        "Before AppRoutedComponent" ,
-                        "After AppRoutedComponent" ,
-                        "Before <App>" ,
+                        "Before <Code>",
+                        "After <Code>",
+                        "Before Zorro",
+                        "After Zorro",
+                        "Before SomeAuthPackage",
+                            "Before PublicPageComponent",
+                            "After PublicPageComponent",
+                            "Before LoginComponent",
+                            "After LoginComponent",
+                            "Before PasswordLostComponent",
+                            "After PasswordLostComponent",
+                            "Before LogoutConfirmComponent",
+                            "After LogoutConfirmComponent",
+                            "Before LogoutResultComponent",
+                            "After LogoutResultComponent",
+                        "After SomeAuthPackage",
+                        "Before PublicSectionComponent",
+                            "Before PublicTopbarComponent",
+                            "After PublicTopbarComponent",
+                            "Before PublicFooterComponent",
+                            "After PublicFooterComponent",
+                        "After PublicSectionComponent",
+                        "Before DemoNgModule",
+                        "After DemoNgModule",
+                        "Before AppRoutedComponent",
+                        "After AppRoutedComponent",
+                        "Before <App>",
                         "After <App>"
                      ] );
+        }
+
+        static ResSpaceConfiguration ExplicitGraph( bool revertOrdering )
+        {
+            var config = new ResSpaceConfiguration();
+            config.RevertOrderingNames = revertOrdering;
+            var dAppRoutedComponent = CreatePackage( config, "AppRoutedComponent" );
+            var dDemoNgModule = CreatePackage( config, "DemoNgModule" );
+            var dPublicPageComponent = CreatePackage( config, "PublicPageComponent" );
+            var dZorro = CreatePackage( config, "Zorro" );
+            var dLogoutConfirmComponent = CreatePackage( config, "LogoutConfirmComponent" );
+            var dLoginComponent = CreatePackage( config, "LoginComponent" );
+            var dLogoutResultComponent = CreatePackage( config, "LogoutResultComponent" );
+            var dPublicFooterComponent = CreatePackage( config, "PublicFooterComponent" );
+            var dPublicTopbarComponent = CreatePackage( config, "PublicTopbarComponent" );
+            var dPasswordLostComponent = CreatePackage( config, "PasswordLostComponent" );
+            var dPublicSectionComponent = CreatePackage( config, "PublicSectionComponent" );
+            var dSomeAuthPackage = CreatePackage( config, "SomeAuthPackage" );
+            dLoginComponent.Requires.Add( dPublicPageComponent );
+            dLogoutResultComponent.Requires.Add( dLogoutConfirmComponent );
+            dPasswordLostComponent.Requires.Add( dLoginComponent );
+            dPublicSectionComponent.Requires.Add( dZorro );
+            dPublicSectionComponent.Children.Add( dPublicTopbarComponent );
+            dPublicSectionComponent.Children.Add( dPublicFooterComponent );
+            dSomeAuthPackage.Children.Add( dPasswordLostComponent );
+            dSomeAuthPackage.Children.Add( dLogoutResultComponent );
+            dSomeAuthPackage.Children.Add( dLogoutConfirmComponent );
+            dSomeAuthPackage.Children.Add( dLoginComponent );
+            return config;
+        }
+
+        static ResSpaceConfiguration FromType( bool revertOrdering )
+        {
+            var config = new ResSpaceConfiguration();
+            config.RevertOrderingNames = revertOrdering;
+            config.RegisterPackage( TestHelper.Monitor, typeof( AppRoutedComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( DemoNgModule ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( PublicPageComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( Zorro ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( LogoutConfirmComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( LoginComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( LogoutResultComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( PublicFooterComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( PublicTopbarComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( PasswordLostComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( PublicSectionComponent ) );
+            config.RegisterPackage( TestHelper.Monitor, typeof( SomeAuthPackage ) );
+            return config;
         }
     }
 
