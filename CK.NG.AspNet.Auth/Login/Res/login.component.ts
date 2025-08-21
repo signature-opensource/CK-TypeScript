0189f11e99c15940e04b5ae2949a4661c9e099f2
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-08-21 18:09:48 +0200

    Finalized login redirection.

--- CK.NG.AspNet.Auth/Login/Res/login.component.ts
+++ CK.NG.AspNet.Auth/Login/Res/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, effect, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { IUserSchemeInfo } from '@local/ck-gen/CK/AspNet/Auth';
 import { NgAuthService } from '../NgAuthService';
@@ -9,88 +9,96 @@ import { ResponsiveDirective } from '@local/ck-gen';
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
-@Component( {
-  selector: 'ck-login',
-  imports: [
-    CommonModule,
-    TranslateModule,
-    ResponsiveDirective,
-    NzButtonModule
-  ],
-  templateUrl: './login.component.html'
-} )
+@Component({
+    selector: 'ck-login',
+    imports: [
+        CommonModule,
+        TranslateModule,
+        ResponsiveDirective,
+        NzButtonModule
+    ],
+    templateUrl: './login.component.html'
+})
 export class LoginComponent implements OnInit {
-  // <PreDependencyInjection revert />
-  readonly #authService = inject( NgAuthService );
-  readonly #router = inject( Router );
-  // <PostDependencyInjection />
+    // <PreDependencyInjection revert />
+    readonly #authService = inject(NgAuthService);
+    readonly #router = inject(Router);
+    readonly #route = inject(ActivatedRoute);
+    readonly #location = inject(Location);
+    // <PostDependencyInjection />
 
-  // TODO: handle light/dark mode toggle
-  // logoWhiteSrc = input<string>( 'logos/login-logo-white.png' );
+    // TODO: handle light/dark mode toggle
+    // logoWhiteSrc = input<string>( 'logos/login-logo-white.png' );
 
-  // assets can be overridden
-  displayedLogoSrc: string = 'logos/login-logo.png';
+    // assets can be overridden
+    displayedLogoSrc: string = 'logos/login-logo.png';
 
-  // <PreLocalVariables revert />
-  // <PreRedirectionPath revert />
-  redirectionPath: string = '';
-  // <PostRedirectionPath />
-  sortedProviders: Array<string> = [];
-  // <PostLocalVariables />
+    // <PreLocalVariables revert />
+    // <PreRedirectionPath revert />
+    redirectionPath: string = '';
+    // <PostRedirectionPath />
+    sortedProviders: Array<string> = [];
+    // <PostLocalVariables />
 
-  constructor() {
-    this.#authService.authService.addOnChange( async ( auth ) => {
-      if ( auth.authenticationInfo.user.userId !== 0 ) {
-        this.#router.navigate( [this.redirectionPath] );
-      }
-    } );
+    constructor() {
+        this.#authService.authService.addOnChange(async (auth) => {
+            if (auth.authenticationInfo.user.userId !== 0) {
+                this.#router.navigate([this.redirectionPath]);
+            }
+        });
 
-    effect( () => {
-      this.sortStringsByLastUsed();
-    } );
-  }
-
-  ngOnInit(): void {
-    if( this.#authService.authenticationInfo().user.userId > 0 ) {
-      this.#router.navigate( [this.redirectionPath] );
-    }
-  }
-
-  async loginWithProvider( p: string ): Promise<void> {
-    await this.#authService.authService.startPopupLogin( p );
-  }
-
-  getProviderLogoSrc( p: string ): string {
-    if ( p.includes( 'Oidc.' ) ) {
-      return `login-providers/${p.split( 'Oidc.' )[1].toLocaleLowerCase()}.png`;
-    }
-    return `login-providers/${p.toLocaleLowerCase()}.png`;
-  }
-
-  getProviderName( p: string ): string {
-    if ( p.includes( 'Oidc.' ) ) {
-      return p.split( 'Oidc.' )[1];
+        effect(() => {
+            this.sortStringsByLastUsed();
+        });
     }
 
-    return p;
-  }
+    ngOnInit(): void {
+        const redirect = this.#route.snapshot.queryParams['redirectTo'];
+        if (redirect) {
+            this.redirectionPath = redirect;
+        }
+        if (this.#authService.authenticationInfo().user.userId > 0) {
+            this.#router.navigate([this.redirectionPath]);
+        } else if (this.redirectionPath !== '') {
+            this.#location.replaceState(this.redirectionPath);
+        }
+    }
 
-  sortStringsByLastUsed(): void {
-    const lastUsedMap = new Map<string, Date>();
-    this.#authService.authenticationInfo().user.schemes.forEach( ( obj: IUserSchemeInfo ) => {
-      lastUsedMap.set( obj.name, obj.lastUsed );
-    } );
+    async loginWithProvider(p: string): Promise<void> {
+        await this.#authService.authService.startPopupLogin(p);
+    }
 
-    const schemes = [...this.#authService.authService.availableSchemes];
-    this.sortedProviders = schemes.sort( ( a, b ) => {
-      const dateA = lastUsedMap.get( a );
-      const dateB = lastUsedMap.get( b );
+    getProviderLogoSrc(p: string): string {
+        if (p.includes('Oidc.')) {
+            return `login-providers/${p.split('Oidc.')[1].toLocaleLowerCase()}.png`;
+        }
+        return `login-providers/${p.toLocaleLowerCase()}.png`;
+    }
 
-      if ( !dateA && !dateB ) return 0;
-      if ( !dateA ) return 1;
-      if ( !dateB ) return -1;
+    getProviderName(p: string): string {
+        if (p.includes('Oidc.')) {
+            return p.split('Oidc.')[1];
+        }
 
-      return dateB.getTime() - dateA.getTime();
-    } );
-  }
+        return p;
+    }
+
+    sortStringsByLastUsed(): void {
+        const lastUsedMap = new Map<string, Date>();
+        this.#authService.authenticationInfo().user.schemes.forEach((obj: IUserSchemeInfo) => {
+            lastUsedMap.set(obj.name, obj.lastUsed);
+        });
+
+        const schemes = [...this.#authService.authService.availableSchemes];
+        this.sortedProviders = schemes.sort((a, b) => {
+            const dateA = lastUsedMap.get(a);
+            const dateB = lastUsedMap.get(b);
+
+            if (!dateA && !dateB) return 0;
+            if (!dateA) return 1;
+            if (!dateB) return -1;
+
+            return dateB.getTime() - dateA.getTime();
+        });
+    }
 }
