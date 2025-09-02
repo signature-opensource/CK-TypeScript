c156a9c09de843ad26c433429fb247083f948eac
Benjamin Crosnier <benjamin.crosnier@signature.one>
2025-09-02 11:12:10 +0200

    Apply: git rm -r --cached . && git add .

--- Tests/Angular/CK.TS.Angular.Tests/TSInlineTests/CK_TS_Angular/ck-gen/CK/Ng/AspNet/Auth/login/routes.ts
+++
@@ -1,5 +0,0 @@
-// import { Route } from '@angular/router';
-
-export default [
-{ path: "password-lost", loadComponent: () => import( "../password-lost/password-lost" ).then( c => c.PasswordLostComponent ) }
-]; // as Route[];
